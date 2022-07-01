aa2a6b9b35fc11912064cfa3035980c5b55fc7d7
BigZoo92 <givernaudenzo@gmail.com>
2022-07-01 14:11:10 +0200

    404 c'est marrant

--- themes/bee-french/404.php
+++ themes/bee-french/404.php
@@ -1,9 +1,12 @@
 <?php get_header() ?>
 
 <main>
+
     <section class="cd_404error">
+        <img src="<?php echo get_template_directory_uri(); ?>/code/images/jpg/shaq_shoes.jpg" alt="Shaq phone shoes" class="hero_404">
         <h1>Page introuvable</h1>
         <h2>Cette page n'existe pas</h2>
+        <p>Tu n'as pas trouvé la paire que tu espérais ? Clique <a href="<?= get_post_type_archive_link('post') ?>">ici</a> pour trouver ton bonheur de snekaer addict</p>
     </section>
 </main>
 
