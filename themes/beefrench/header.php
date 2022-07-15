c3cebdaa801493482cb4201bafb289bd528de74a
matheolienard <matheolienard@gmail.com>
2022-07-15 17:02:12 +0200

    init

---
+++ themes/beefrench/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beefrench</title>
+    <?php wp_head(  )?>
+</head>
+
+<body>
+
+    <body>
+        <div class="marges" data-scroll-container>
+            <header>
+                <nav>
+                    <a href="index.html">
+                        <h5>BEEFRENCH</h5>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="produit.html">
+                                SHOES
+                            </a>
+                        </li>
+                        <li>
+                            <a href="fiche_produit.html">
+                                APPAREL
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-bag" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="toggle_menu">
+                        <div class="arm_menu_1"></div>
+                        <div class="arm_menu_2"></div>
+                        <div class="arm_menu_3"></div>
+                    </div>
+                </nav>
+            </header>
+            <div class="dark_overlay_header"></div>
+            <section class="header2">
+                <nav>
+                    <ul>
+                        <li class="li_toggle">
+                            <a href="produit.html">
+                                SHOES
+                            </a>
+                        </li>
+                        <li class="li_toggle">
+                            <a href="fiche_produit.html">
+                                APPAREL
+                            </a>
+                        </li>
+                        <li class="li_toggle">
+                            <a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                    class="bi bi-bag" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+            </section>
