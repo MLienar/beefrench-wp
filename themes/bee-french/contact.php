70447db51f4f65a96f5120acd4d264efde63c135
BigZoo92 <givernaudenzo@gmail.com>
2022-09-26 20:30:14 +0200

    rectifications demandées

--- themes/bee-french/contact.php
+++ themes/bee-french/contact.php
@@ -7,6 +7,13 @@
 
 get_header(); ?>
 <main>
+<div class="txt_gall">
+    <h2>COLLECTIONS CUSTOMS</h2>
+    <p>Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Fuga veniam pariatur a, iste, id
+        tempore, molestiae corrupti ad repellendus cumque nesciunt eius mollitia consectetur
+        perspiciatis. Itaque adipisci officiis inventore odit.</p>
+</div>
     <?= do_shortcode('[contact-form-7 id="291" title="Contact form 1"]'); ?>
 </main>
 <?php get_footer(); ?>
